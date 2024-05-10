0fab516ed864d98d4e7672a68aaa4b126a97bf63
如月十八 <1911834200@qq.com>
2024-05-10 11:48:22 +0800

    feat: 文案信息修改

--- pages/meaning.vue
+++ pages/meaning.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h1 class="text-center my-10">基于决策树的关键特征分析</h1>
-    <span class="whitespace-pre-line text-gray-500 text-xl indent-md my-10">{{ descStr }}</span>
+    <div class="text-gray-500 text-xl indent-md my-10">
+      &nbsp;&nbsp;决策树算法的可解释性较好，主要是因为其生成的模型可以被直观地解释和理解。决策树通过数据特征来对每一个样本进行划分，生成一棵树形结构。这个树形结构可以被视为一系列简单的判断规则，可以帮助人们理解模型是如何做出决策的。
+      <br />
+      &nbsp;&nbsp;相比之下，其他传统机器学习算法生成的模型通常更复杂，难以被直观地解释和理解，即便使用sklearn分析在分类过程中发挥关键作用的特征，这些信息也不具有生物学意义上的可解释性。例如，SVM生成的模型是一个超平面，随机森林生成的模型是一组决策树的集合，KNN算法的模型本质上是一组训练数据集中的实例点，而不是一个明确的模型或规则。神将网络算法相比传统机器学习算法更加黑盒，可解释性也较差。
+      <br />
+      &nbsp;&nbsp;基于以上原因，本文使用可视化方式分析决策树算法获得的关键特征。
+    </div>
+
     <TotalLine />
 
     <h1 class="text-center my-10">不同规模下数据集关键特征</h1>
@@ -19,7 +26,7 @@
     <Dot />
 
     <h1 class="text-center my-10">关键特征对应生物学意义</h1>
-    <DescribeTable :info="meaning" :title="['正交群', '生物学意义', '对应基因序列']" />
+    <DescribeTable :info="meaning" :title="['基因家族', '生物学含义']" />
   </div>
 </template>
 
@@ -35,7 +42,7 @@ const update = (num: number) => {
 };
 
 const descStr =
-  'DecisionTreeClassifier() 和 RandomForestClassifier() 都能获得关键特征索引和值，因为它们是基于决策树的算法，决策树本身就是一种解释性较好的算法。在决策树中，每个节点都代表一个特征，节点的分裂依据就是该特征的值，因此可以通过查看决策树的结构来获得关键特征的信息。\n\nKNeighborsClassifier()、GaussianNB()、MLPClassifier() 和 SVC() 都不能直接获得关键特征索引和值，因为它们是基于统计学习的算法，无法直接解释每个特征的重要性。但是可以通过一些可视化工具和特征选择方法来辅助分析关键特征。\n\n总的来说，如果需要获得关键特征的信息并且需要算法具有较好的解释性，建议选择基于决策树的算法，如 DecisionTreeClassifier() 和 RandomForestClassifier()。';
+  '决策树算法的可解释性较好，主要是因为其生成的模型可以被直观地解释和理解。决策树通过数据特征来对每一个样本进行划分，生成一棵树形结构。这个树形结构可以被视为一系列简单的判断规则，可以帮助人们理解模型是如何做出决策的。\n相比之下，其他传统机器学习算法生成的模型通常更复杂，难以被直观地解释和理解，即便使用sklearn分析在分类过程中发挥关键作用的特征，这些信息也不具有生物学意义上的可解释性。例如，SVM生成的模型是一个超平面，随机森林生成的模型是一组决策树的集合，KNN算法的模型本质上是一组训练数据集中的实例点，而不是一个明确的模型或规则。神将网络算法相比传统机器学习算法更加黑盒，可解释性也较差。\n基于以上原因，本文使用可视化方式分析决策树算法获得的关键特征';
 const meaning = useData('meaning', cur.value);
 </script>
 
